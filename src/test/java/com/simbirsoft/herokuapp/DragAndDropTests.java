0d0e1c2e5a5a83105979e5dea64b03b5d00d413d
AleksandrDankov <dankoffalexander@gmail.com>
2022-06-17 01:14:34 +0500

    Added tests to DragAndDropTests

--- src/test/java/com/simbirsoft/herokuapp/DragAndDropTests.java
+++ src/test/java/com/simbirsoft/herokuapp/DragAndDropTests.java
@@ -1,4 +1,18 @@
 package com.simbirsoft.herokuapp;
 
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
 public class DragAndDropTests {
+    @Test
+    public void rectanglesShouldSwapPlaces() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        $("#columns").$("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A"));
+
+    }
 }
